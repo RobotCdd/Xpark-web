4637b342e561b0d716b9deba6ebe6efd636ef1de
J <jackc.foh@gmail.com>
2025-07-04 01:49:00 +0100

    Stuff

--- website/website/settings.py
+++ website/website/settings.py
@@ -21,11 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$y#bqh*^76gr29$gb=d5dj^&obrtvibz_ytv8v@jg6e%i0j7xl'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "website-hidden-glade-7213.fly.dev",
